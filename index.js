672ab715826a59b5efa48ec3c541c7ddb79decb4
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-22 19:22:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ function nameToAttributes(drivers) {
     const first = driver.split(" ")[0];
     const last = driver.split(" ")[1];
     
-    
-    
+    return { firstName: first, lastName: last };
   );
-
-  
 } 
+
